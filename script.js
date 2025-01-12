2d1dc2b8f7171c084018a5e7620a9e8117ef2396
Harryrana98 <ranaharendra77@gmail.com>
2025-01-12 10:12:34 +0530

    updated

---
+++ script.js
@@ -0,0 +1,35 @@
+let button = document.querySelector("body");
+function purple() {
+  button.style = "background: purple";
+  const purple = document.querySelector("#purple");
+  purple.style.backgroundColor = "purple";
+  purple.style.color = "white";
+}
+
+function red() {
+  button.style.backgroundColor = "red";
+  const red = document.querySelector("#red");
+  red.style.backgroundColor = "red";
+  red.style.color = "white";
+}
+
+function green() {
+  button.style.backgroundColor = "green";
+  const green = document.querySelector("#green");
+  green.style.backgroundColor = "green";
+  green.style.color = "white";
+}
+
+function blue() {
+  button.style.backgroundColor = "blue";
+  const blue = document.querySelector("#blue");
+  blue.style.backgroundColor = "blue";
+  blue.style.color = "white";
+}
+
+function maroon() {
+  button.style.backgroundColor = "maroon";
+  const maroon = document.querySelector("#maroon");
+  maroon.style.backgroundColor = "maroon";
+  maroon.style.color = "white";
+}
